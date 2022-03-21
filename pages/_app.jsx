6fc634820b4db71bb44234a39b2562a5789b6403
Paolo <15671939+paologhidoni@users.noreply.github.com>
2022-03-21 14:32:49 +0000

    Add chakra UI - relates #16
    * Installed chakra UI
    * Created login page
    * Created themes.ts file inside styles folder to create custom themes
    * Started creating custom theme

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+
+//import custom theme
+import { myNewTheme } from "../styles/themes.ts";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={myNewTheme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,55 @@
-import Link from "next/link";
+// import Link from "next/link";
+
+import {
+  Button,
+  Flex,
+  Heading,
+  Input,
+  useColorMode,
+  useColorModeValue,
+} from "@chakra-ui/react";
 
 export default function Home() {
+  const { toggleColorMode } = useColorMode();
+  const formBackground = useColorModeValue("gray.100", "gray.700");
+
   return (
-    <>
-      <Link href="/login" passHref>
-        <button className="button">Log in</button>
-      </Link>
-      <Link href="/signup" passHref>
-        <button className="button">Sign up</button>
-      </Link>
-    </>
+    <Flex height="100vh" alignItems="center" justifyContent="center">
+      <Flex direction="column" background={formBackground} p={12} rounded={10}>
+        <Heading mb={6}>Log In</Heading>
+        <Input
+          placeholder="Please enter your email"
+          variant="filled"
+          mb={3}
+          type="email"
+        ></Input>
+        <Input
+          placeholder="*****"
+          variant="filled"
+          mb={6}
+          type="password"
+        ></Input>
+        <Button mb={6} colorScheme="teal">
+          Log in
+        </Button>
+        <Button mb={6} colorScheme="teal" bg="primary">
+          Primary color
+        </Button>
+        <Button onClick={toggleColorMode}>Toggle Color Mode</Button>
+      </Flex>
+    </Flex>
   );
 }
+
+// export default function Home() {
+//   return (
+//     <>
+//       <Link href="/login" passHref>
+//         <button className="button">Log in</button>
+//       </Link>
+//       <Link href="/signup" passHref>
+//         <button className="button">Sign up</button>
+//       </Link>
+//     </>
+//   );
+// }
