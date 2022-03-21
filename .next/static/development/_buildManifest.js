250f847725c827bb637a12bba6c778a8336746de
Adam <31065348+adam8-9@users.noreply.github.com>
2022-03-21 14:16:30 +0000

    Delete .next directory

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/_middleware":["server\u002Fmiddleware-runtime.js","server\u002Fpages\u002F_middleware.js"],sortedPages:["\u002F_app","\u002F_error","\u002F_middleware"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
