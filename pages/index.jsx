879f98f6ebc533df26423449eaf40e5459da4b58
Paolo <15671939+paologhidoni@users.noreply.github.com>
2022-03-21 17:08:06 +0000

    Create Navigation and DarkModeSwitch components - relates #16 closes #27
    * Created Navigation component
    * Created DarkModeSwitch component
    * Created resources page
    * Created myprofile page
    * Modified global.css inside styles folder to modify chakra UI classes
    
    Co-authored-by: <Oli 78092825+duckRabbitPy@users.noreply.github.com>
    Co-authored-by: <Adam 31065348+adam8-9@users.noreply.github.com>
    Co-authored-by: <Paolo 15671939+paologhidoni@users.noreply.github.com>

--- pages/index.jsx
+++ pages/index.jsx
@@ -15,20 +15,27 @@ export default function Home() {
   const formBackground = useColorModeValue("gray.100", "gray.700");
 
   return (
-    <Flex height="100vh" alignItems="center" justifyContent="center">
-      <Flex direction="column" background={formBackground} p={12} rounded={10}>
-        <Heading mb={10}>Exam Grade Boost</Heading>
-        <Link href="/login" passHref mb={6}>
-          <Button className="button" colorScheme="yellow" mb={6}>
-            Log in
-          </Button>
-        </Link>
-        <Link href="/signup" passHref>
-          <Button className="button" colorScheme="orange">
-            Sign up
-          </Button>
-        </Link>
+    <>
+      <Flex height="100vh" alignItems="center" justifyContent="center">
+        <Flex
+          direction="column"
+          background={formBackground}
+          p={12}
+          rounded={10}
+        >
+          <Heading mb={10}>Exam Grade Boost</Heading>
+          <Link href="/login" passHref mb={6}>
+            <Button className="button" colorScheme="yellow" mb={6}>
+              Log in
+            </Button>
+          </Link>
+          <Link href="/signup" passHref>
+            <Button className="button" colorScheme="orange">
+              Sign up
+            </Button>
+          </Link>
+        </Flex>
       </Flex>
-    </Flex>
+    </>
   );
 }
---
+++ pages/myprofile.jsx
@@ -0,0 +1,16 @@
+// import chakra UI components
+import { Container } from "@chakra-ui/react";
+
+// import components
+import Navigation from "../components/Navigation.jsx";
+
+export default function MyProfile() {
+  return (
+    <>
+      <Navigation />
+      <Container>
+        <h1>My Profile page</h1>
+      </Container>
+    </>
+  );
+}
