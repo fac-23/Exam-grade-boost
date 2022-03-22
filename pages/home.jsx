0559255c4c251d0e3f917b588ef7ab9b189c60f5
adam8-9 <adambhuhati456@gmail.com>
2022-03-22 16:30:40 +0000

    started layout for home page

--- pages/home.jsx
+++ pages/home.jsx
@@ -1,5 +1,18 @@
 import { getSessionInfo, getContactInfo } from "../database/model";
+
+import {
+  Button,
+  Flex,
+  Heading,
+  FormLabel,
+  useColorMode,
+  useColorModeValue,
+  Box,
+  Text
+} from "@chakra-ui/react";
+
 import Navigation from "../components/Navigation.jsx";
+
 
 export async function getServerSideProps({ req }) {
   const userData = await getSessionInfo(req.cookies.sid);
@@ -20,10 +33,16 @@ export async function getServerSideProps({ req }) {
 }
 
 export default function Home({ user_id, username, email, cookie }) {
+  const boxBorder = useColorModeValue("gray.100", "gray.700");
+
   return (
     <>
       <Navigation />
-      <div>
+      <Flex mt={10} alignItems="flex-start" justifyContent="center" direction="row">
+      <Flex mr={20} direction="column">
+     
+      <Heading m="2rem">Welcome back Jane!</Heading>
+      {/* <div>
         <p>Logged in as: {username} </p>
         <p>User id is: {user_id}</p>
         <p>Email is: {email}</p>
@@ -32,7 +51,31 @@ export default function Home({ user_id, username, email, cookie }) {
         <form method="POST" action="/api/log-out">
           <button id="logout">Log out</button>
         </form>
-      </div>
+      </div> */}
+      <Button mb="2rem"> Create new Essay</Button>
+
+
+      <Box m="1rem">
+      <Heading mb="1rem">Completed Essays</Heading>    
+      <Box m={3} h="7rem" w="100%" p={5} borderColor="black" borderWidth="2px"><Text>How far do you agree,UK roads are easy to travel on?</Text></Box>
+      <Box m={3} h="7rem" w="100%" p={5} borderColor="black" borderWidth="2px"><Text>The representation of madness in Shakespeare's text,Hamlet</Text></Box>
+      <Box m={3} h="7rem" w="100%" p={5} borderColor="black" borderWidth="2px"><Text>Wilde's Critique of Victorian Society in The Importance of Being Earnest</Text></Box>
+      </Box>
+
+      <Box m="1rem">
+      <Heading mb="1rem">Draft Essays</Heading>
+      <Box m={3} h="7rem" w="100%" borderColor="black"  borderWidth="2px"><Text>How far do you agree,UK roads are easy to travel on?</Text></Box>
+      <Box m={3} h="7rem" w="100%" borderColor="black"  borderWidth="2px"><Text>The representation of madness in Shakespeare's text,Hamlet</Text></Box>
+      <Box m={3} h="7rem" w="100%" borderColor="black"  borderWidth="2px"><Text>Wilde's Critique of Victorian Society in The Importance of Being Earnest</Text></Box>
+     </Box>
+      </Flex >
+
+    <Flex m="2rem" w="20%"  alignItems="center" justifyContent="center" direction="column">
+      <Heading mb="1rem">Profile</Heading>
+      <Box h="40rem" w="100%" borderColor="black"  borderWidth="2px"></Box>
+     </Flex>
+
+      </Flex>
     </>
   );
 }
