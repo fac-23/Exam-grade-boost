64fec7658cc59b3bf830f318965cadae1e3f4f0b
Oli Jones <78092825+duckRabbitPy@users.noreply.github.com>
2022-03-22 16:52:45 +0000

    Update home.jsx
    Co-Authored-By: Paolo Ghidoni <15671939+paologhidoni@users.noreply.github.com>
    Co-Authored-By: Adam <31065348+adam8-9@users.noreply.github.com>

--- pages/home.jsx
+++ pages/home.jsx
@@ -1,18 +1,16 @@
 import { getSessionInfo, getContactInfo } from "../database/model";
+import Link from "next/link";
 
 import {
   Button,
   Flex,
   Heading,
-  FormLabel,
-  useColorMode,
   useColorModeValue,
   Box,
-  Text
+  Text,
 } from "@chakra-ui/react";
 
 import Navigation from "../components/Navigation.jsx";
-
 
 export async function getServerSideProps({ req }) {
   const userData = await getSessionInfo(req.cookies.sid);
@@ -32,49 +30,95 @@ export async function getServerSideProps({ req }) {
   };
 }
 
-export default function Home({ user_id, username, email, cookie }) {
+export default function Home({ username }) {
   const boxBorder = useColorModeValue("gray.100", "gray.700");
 
   return (
     <>
       <Navigation />
-      <Flex mt={10} alignItems="flex-start" justifyContent="center" direction="row">
-      <Flex mr={20} direction="column">
-     
-      <Heading m="2rem">Welcome back Jane!</Heading>
-      {/* <div>
-        <p>Logged in as: {username} </p>
-        <p>User id is: {user_id}</p>
-        <p>Email is: {email}</p>
-        <p>SID cookie is: {cookie}</p>
+      <Flex
+        mt={10}
+        alignItems="flex-start"
+        justifyContent="center"
+        direction="row"
+      >
+        <Flex mr={20} direction="column">
+          <Heading m="2rem">Welcome back {username}</Heading>
 
-        <form method="POST" action="/api/log-out">
-          <button id="logout">Log out</button>
-        </form>
-      </div> */}
-      <Button mb="2rem"> Create new Essay</Button>
+          <Link href="/newEssay" passHref>
+            <Button mb="2rem"> Create new Essay</Button>
+          </Link>
 
+          <Box m="1rem">
+            <Heading mb="1rem">Completed Essays</Heading>
+            <Box
+              m={3}
+              h="7rem"
+              w="100%"
+              p={5}
+              borderColor="black"
+              borderWidth="2px"
+            >
+              <Text>How far do you agree,UK roads are easy to travel on?</Text>
+            </Box>
+            <Box
+              m={3}
+              h="7rem"
+              w="100%"
+              p={5}
+              borderColor="black"
+              borderWidth="2px"
+            >
+              <Text>
+                The representation of madness in Shakespeares text, Hamlet
+              </Text>
+            </Box>
+            <Box
+              m={3}
+              h="7rem"
+              w="100%"
+              p={5}
+              borderColor="black"
+              borderWidth="2px"
+            >
+              <Text>
+                Wildes Critique of Victorian Society in The Importance of Being
+                Earnest
+              </Text>
+            </Box>
+          </Box>
 
-      <Box m="1rem">
-      <Heading mb="1rem">Completed Essays</Heading>    
-      <Box m={3} h="7rem" w="100%" p={5} borderColor="black" borderWidth="2px"><Text>How far do you agree,UK roads are easy to travel on?</Text></Box>
-      <Box m={3} h="7rem" w="100%" p={5} borderColor="black" borderWidth="2px"><Text>The representation of madness in Shakespeare's text,Hamlet</Text></Box>
-      <Box m={3} h="7rem" w="100%" p={5} borderColor="black" borderWidth="2px"><Text>Wilde's Critique of Victorian Society in The Importance of Being Earnest</Text></Box>
-      </Box>
+          <Box m="1rem">
+            <Heading mb="1rem">Draft Essays</Heading>
+            <Box m={3} h="7rem" w="100%" borderColor="black" borderWidth="2px">
+              <Text>
+                How far do you agree that UK roads are easy to travel on?
+              </Text>
+            </Box>
+            <Box m={3} h="7rem" w="100%" borderColor="black" borderWidth="2px">
+              <Text>
+                The representation of madness in Shakespeares text, Hamlet
+              </Text>
+            </Box>
+            <Box m={3} h="7rem" w="100%" borderColor="black" borderWidth="2px">
+              <Text>
+                Wildes Critique of Victorian Society in The Importance of Being
+                Earnest
+              </Text>
+            </Box>
+          </Box>
+        </Flex>
 
-      <Box m="1rem">
-      <Heading mb="1rem">Draft Essays</Heading>
-      <Box m={3} h="7rem" w="100%" borderColor="black"  borderWidth="2px"><Text>How far do you agree,UK roads are easy to travel on?</Text></Box>
-      <Box m={3} h="7rem" w="100%" borderColor="black"  borderWidth="2px"><Text>The representation of madness in Shakespeare's text,Hamlet</Text></Box>
-      <Box m={3} h="7rem" w="100%" borderColor="black"  borderWidth="2px"><Text>Wilde's Critique of Victorian Society in The Importance of Being Earnest</Text></Box>
-     </Box>
-      </Flex >
-
-    <Flex m="2rem" w="20%"  alignItems="center" justifyContent="center" direction="column">
-      <Heading mb="1rem">Profile</Heading>
-      <Box h="40rem" w="100%" borderColor="black"  borderWidth="2px"></Box>
-     </Flex>
-
+        <Flex
+          m="2rem"
+          w="20%"
+          alignItems="center"
+          justifyContent="center"
+          direction="column"
+        >
+          <Heading mb="1rem">Profile</Heading>
+          <Box h="40rem" w="100%" borderColor="black" borderWidth="2px"></Box>
+        </Flex>
       </Flex>
     </>
   );
