f218a33ec0436e0fabcce1811ae9467278a1485a
Paolo <15671939+paologhidoni@users.noreply.github.com>
2022-03-22 17:48:59 +0000

    Add Docxtemplater and setup - relates #15
    * Installed Docxtemplater package.
    * Installed file-saver package.
    * Set up button to download MS DOC.
    * Started setting up Docxtemplater but still not working, need to
    create a Microsoft Word template to store in our project and then
    replace the tag inside the template with the essay copy.
    
    Co-authored-by: <Oli 78092825+duckRabbitPy@users.noreply.github.com>
    Co-authored-by: <Adam 31065348+adam8-9@users.noreply.github.com>

--- pages/finalEssay.jsx
+++ pages/finalEssay.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import {
   Flex,
   Textarea,
@@ -14,6 +15,11 @@ import { getEssayInfo } from "../database/model.js";
 
 // import jsPDF
 import { jsPDF } from "jspdf";
+
+// import and set up Docxtemplater
+import Docxtemplater from "docxtemplater";
+import PizZip from "pizzip";
+import { saveAs } from "file-saver";
 
 export async function getServerSideProps({ req }) {
   // gets the essay id from the cookie
@@ -59,20 +65,67 @@ export default function finalEssay({
   storedOpposite,
   storedKey,
 }) {
-  // jsPDF
+  // *** jsPDF functionality *** //
 
+  // Create the final essay content adding all sections
   const finalEssayCopy = `${storedSummary}\n\n${storedMain}\n\n${storedOpposite}\n\n${storedKey}`;
 
+  // create new instance of a document
   const doc = new jsPDF();
 
+  // set the document text to be the content of the finalEssayCopy variable
   doc.text(finalEssayCopy, 10, 10);
 
   // callback function to pass onClick event on export to PDF button
   function downloadPDF() {
+    // set the file name to be the question and download the file
     doc.save(`${question.split(",")}`);
   }
 
   // console.log("FINAL ESSAY", finalEssayCopy);
+
+  // Docxtemplater ***********************************************/
+
+  let PizZipUtils = null;
+  if (typeof window !== "undefined") {
+    import("pizzip/utils/index.js").then(function (r) {
+      PizZipUtils = r;
+    });
+  }
+
+  function loadFile(url, callback) {
+    PizZipUtils.getBinaryContent(url, callback);
+  }
+
+  const generateDocument = () => {
+    loadFile(
+      "https://docxtemplater.com/tag-example.docx",
+      function (error, content) {
+        if (error) {
+          throw error;
+        }
+        const zip = new PizZip(content);
+        const doc = new Docxtemplater().loadZip(zip);
+        // render the document (replace all occurences of {first_name} by John, {last_name} by Doe, ...)
+        doc.render({
+          first_name: "Mannie",
+          last_name: "Doe",
+          phone: "0652455478",
+          description: "New Website",
+        });
+        doc.render({ finalEssayCopy });
+        const out = doc.getZip().generate({
+          type: "blob",
+          mimeType:
+            "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+        });
+        // Output the document using Data-URI
+        saveAs(out, "output.docx");
+      }
+    );
+  };
+
+  // ***********************************************/
 
   return (
     <>
@@ -90,7 +143,11 @@ export default function finalEssay({
             </section>
           </Flex>
           <Flex justifyContent="center" gap="2rem">
-            <Button className="button" colorScheme="teal">
+            <Button
+              className="button"
+              colorScheme="teal"
+              onClick={generateDocument}
+            >
               Export to Word docx
             </Button>
             <Button className="button" colorScheme="teal" onClick={downloadPDF}>
