c9842cb989f0f295a9cc64ca67ca1d8a598f80e4
Paolo <15671939+paologhidoni@users.noreply.github.com>
2022-03-28 15:31:05 +0100

    Build layout for resources page and each resource page - relates #12
    Co-authored-by: <Oli 78092825+duckRabbitPy@users.noreply.github.com>
    Co-authored-by: <Adam 31065348+adam8-9@users.noreply.github.com>

--- pages/resources.jsx
+++ pages/resources.jsx
@@ -1,16 +1,43 @@
+import Link from "next/link";
+
 // import chakra UI components
-import { Container, Heading } from "@chakra-ui/react";
+import { Container, Heading, Flex } from "@chakra-ui/react";
+
+// import components
 import Layout from "../components/Layout.jsx";
+
+// import icons
+import { InfoIcon } from "@chakra-ui/icons";
 
 export default function Resources() {
   return (
     <Layout>
-      <Container>
-        <Heading>Resources</Heading>
-        <p>Videos</p>
-        <p>Word bank</p>
-        <p>Spider diagrams</p>
-        <p>Guides</p>
+      <Container centerContent>
+        <Heading as="h1" mb="2rem">
+          Resources
+        </Heading>
+        <Flex direction={["column", "column", "row"]} gap="2rem">
+          <Link href="/videos">
+            <a>Videos</a>
+          </Link>
+
+          <Link href="/wordBank">
+            <a>
+              <InfoIcon mr="1rem" />
+              Word bank
+            </a>
+          </Link>
+
+          <Link
+            href="https://drive.google.com/drive/folders/1IA7eNTex7A82rrIdz0vS5e4m20mCiPap"
+            passHref
+          >
+            <a target="_blank">
+              <InfoIcon mr="1rem" />
+              Guides
+            </a>
+          </Link>
+        </Flex>
       </Container>
     </Layout>
   );
