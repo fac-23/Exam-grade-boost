da0c5298da8a8c3a76b49341d35b5d0dabb3a3dd
adam8-9 <adambhuhati456@gmail.com>
2022-03-29 17:47:44 +0100

    made the kep as branches in spider-diagram

--- pages/api/spider-diagram.js
+++ pages/api/spider-diagram.js
@@ -3,11 +3,23 @@ import Cookies from "cookies";
 
 export default async function getSpiderDiagram(req, res) {
   console.log(req.body);
-  const { branch1, branch2, branch3, branch4 } = req.body;
+  const { 
+    branch1,branch1_key,branch1_agree,branch1_disagree,
+    branch2,branch2_key,branch2_agree,branch2_disagree, 
+    branch3,branch3_key,branch3_agree,branch3_disagree,
+    branch4,branch4_key,branch4_agree,branch4_disagree 
+  } = req.body;
+  
   const essayId = req.cookies.currEssay;
 
-  const planningText =
-    branch1 + "\n" + branch2 + "\n" + branch3 + "\n" + branch4;
+  const topic1 = branch1 + '\n' + branch1_key + '\n' + branch1_agree + '\n' + branch1_disagree
+  const topic2 = branch2 + '\n' + branch2_key + '\n' + branch2_agree + '\n' + branch2_disagree
+  const topic3 = branch3 + '\n' + branch3_key + '\n' + branch3_agree + '\n' + branch3_disagree
+  const topic4 = branch4 + '\n' + branch4_key + '\n' + branch4_agree + '\n' + branch4_disagree
+  
+  const planningText = {topic1: topic1 ,topic2:topic2 , topic3:topic3, topic4:topic4 }
+
+console.log(planningText)
 
   const returnedEssayId = await saveSpiderDiagram(planningText, essayId);
 
--- pages/spiderDiagram.jsx
+++ pages/spiderDiagram.jsx
@@ -28,11 +28,44 @@ export async function getServerSideProps({ req }) {
     };
   }
 
-  const splitSections = storedSpiderText.split("\n");
-  const storedBranch1 = splitSections[0];
-  const storedBranch2 = splitSections[1];
-  const storedBranch3 = splitSections[2];
-  const storedBranch4 = splitSections[3];
+ // const splitSections = storedSpiderText.split("\n");
+
+  const storedBranch1 = storedSpiderText[0].split("\n");
+
+  const topic1 = {}
+
+  storedBranch1.forEach((item,index)=>{
+    let keyName = 'sb'+ index
+    topic1[keyName] = item
+  })
+  
+  console.log('stpt',storedSpiderText)
+  console.log('topic1',topic1)
+
+  // const topic1_key = storedBranch1[1]
+  // const topic1_evidence = storedBranch1[2]
+  // const topic1_point = storedBranch1[3]
+
+   const storedBranch2 = storedSpiderText[1].split("\n");
+
+  // const topic2 = storedBranch2[0]
+  // const topic2_key = storedBranch2[1]
+  // const topic2_evidence = storedBranch2[2]
+  // const topic2_point = storedBranch2[3]
+
+   const storedBranch3 = storedSpiderText[2].split("\n");
+
+  // const topic3 = storedBranch3[0]
+  // const topic3_key = storedBranch3[1]
+  // const topic3_evidence = storedBranch3[2]
+  // const topic3_point = storedBranch3[3]
+
+   const storedBranch4 = storedSpiderText[3].split("\n");
+
+  // const topic4 = storedBranch4[0]
+  // const topic4_key = storedBranch4[1]
+  // const topic4_evidence = storedBranch4[2]
+  // const topic4_point = storedBranch4[3]
 
   return {
     props: {
@@ -69,26 +102,99 @@ export default function SpiderDiagram({
         h="90vh"
       >
         <form method="POST" action="/api/spider-diagram">
-          <Flex direction="row">
-            <Flex justify="center" alignItems="center" direction="row">
-              <Textarea
-                name="branch1"
-                placeholder="branch1"
+
+      <Flex direction="column" alignContent="center" justifyContent="center">
+
+      {/*top item in flex box*/ }
+        <Flex direction="row" alignContent="center" justifyContent="center" mr='2.5rem'>
+            <Textarea
+                name="branch2_key"
+                placeholder="key"
                 defaultValue={storedBranch1 ? storedBranch1 : ""}
-                m={10}
+                m='0 2.5rem 0 0'
+                borderColor="orange.300"
+                visibility={
+                  topicBranch2 || storedBranch2 ? "visible" : "hidden"
+                }
+              ></Textarea>
+               <Textarea
+                name="branch2_agree"
+                placeholder="agree"
+                defaultValue={storedBranch1 ? storedBranch1 : ""}
+                borderColor="orange.300"
+                visibility={
+                  topicBranch2 || storedBranch2 ? "visible" : "hidden"
+                }
+              ></Textarea>
+               <Textarea
+                name="branch2_disagree"
+                placeholder="disagree"
+                defaultValue={storedBranch1 ? storedBranch1 : ""}
+                m='0 0 0 2.5rem'
+                borderColor="orange.300"
+                visibility={
+                  topicBranch2 || storedBranch2 ? "visible" : "hidden"
+                }
+              ></Textarea>
+            </Flex>
+
+ {/*2nd item in flex box*/ }
+          <Flex direction="row">
+
+             {/*most left item in 2nd flex box*/ }
+            <Flex direction="column" alignContent="center" justifyContent="center">
+            <Textarea
+                name="branch1_key"
+                placeholder="key"
+                defaultValue={storedBranch1 ? storedBranch1 : ""}
+                m='0 0 2.5rem 0'
                 borderColor="orange.300"
                 visibility={
                   topicBranch1 || storedBranch1 ? "visible" : "hidden"
                 }
               ></Textarea>
-              <Button w="10%" onClick={() => setTopicBranch1(true)}>
+               <Textarea
+                name="branch1_agree"
+                placeholder="agree"
+                defaultValue={storedBranch1 ? storedBranch1 : ""}
+                borderColor="orange.300"
+                visibility={
+                  topicBranch1 || storedBranch1 ? "visible" : "hidden"
+                }
+              ></Textarea>
+               <Textarea
+                name="branch1_disagree"
+                placeholder="disagree"
+                defaultValue={storedBranch1 ? storedBranch1 : ""}
+                m='2.5rem 0 0 0'
+                borderColor="orange.300"
+                visibility={
+                  topicBranch1 || storedBranch1 ? "visible" : "hidden"
+                }
+              ></Textarea>
+            </Flex>
+
+            {/*2nd left item in 2nd flex box*/ }
+            <Flex mr='1rem' justify="center" alignItems="center" direction="row">
+              <Textarea
+                name="branch1"
+                placeholder="branch1"
+                defaultValue={storedBranch1 ? storedBranch1 : ""}
+                m='0 2.5rem 0 2.5rem'
+                borderColor="orange.300"
+                visibility={
+                  topicBranch1 || storedBranch1 ? "visible" : "hidden"
+                }
+              ></Textarea>
+              <Button  onClick={() => setTopicBranch1(true)}>
                 <Flex alignContent="center" justifyContent="center">
                   <AddIcon />
                 </Flex>
               </Button>
             </Flex>
+
+            {/*middle item in 2nd flex box*/ }
             <Flex
-              m={10}
               justify="center"
               alignItems="center"
               direction="column"
@@ -97,13 +203,13 @@ export default function SpiderDiagram({
                 name="branch2"
                 placeholder="branch2"
                 defaultValue={storedBranch2 ? storedBranch2 : ""}
-                m={10}
+                m='2.5rem'
                 borderColor="orange.300"
                 visibility={
                   topicBranch2 || storedBranch2 ? "visible" : "hidden"
                 }
               ></Textarea>
-              <Button w="10%" onClick={() => setTopicBranch2(true)}>
+              <Button onClick={() => setTopicBranch2(true)}>
                 <Flex alignContent="center" justifyContent="center">
                   <AddIcon />
                 </Flex>
@@ -120,7 +226,7 @@ export default function SpiderDiagram({
               >
                 {question}
               </Box>
-              <Button w="10%" onClick={() => setTopicBranch3(true)}>
+              <Button onClick={() => setTopicBranch3(true)}>
                 <Flex alignContent="center" justifyContent="center">
                   <AddIcon />
                 </Flex>
@@ -129,15 +235,19 @@ export default function SpiderDiagram({
                 name="branch3"
                 placeholder="branch3"
                 defaultValue={storedBranch3 ? storedBranch3 : ""}
-                m={10}
+                m='2.5rem'
                 borderColor="orange.300"
                 visibility={
                   topicBranch3 || storedBranch3 ? "visible" : "hidden"
                 }
               ></Textarea>
             </Flex>
-            <Flex justify="center" alignItems="center" direction="row">
-              <Button w="10%" onClick={() => setTopicBranch4(true)}>
+
+
+          {/*4th item in 2nd flex box*/ }
+
+            <Flex ml='1rem' justify="center" alignItems="center" direction="row">
+              <Button onClick={() => setTopicBranch4(true)}>
                 <Flex alignContent="center" justifyContent="center">
                   <AddIcon />
                 </Flex>
@@ -146,19 +256,90 @@ export default function SpiderDiagram({
                 name="branch4"
                 placeholder="branch4"
                 defaultValue={storedBranch4 ? storedBranch4 : ""}
-                m={10}
+                m='0 2.5rem 0 2.5rem'
                 borderColor="orange.300"
                 visibility={
                   topicBranch4 || storedBranch4 ? "visible" : "hidden"
                 }
               ></Textarea>
             </Flex>
+
+            {/*most right item in 2nd flex box*/ }
+            <Flex direction="column" alignContent="center" justifyContent="center" ml='2.5rem'>
+            <Textarea
+                name="branch4_key"
+                placeholder="key"
+                defaultValue={storedBranch1 ? storedBranch1 : ""}
+                m='0 0 2.5rem 0'
+                borderColor="orange.300"
+                visibility={
+                  topicBranch4 || storedBranch4 ? "visible" : "hidden"
+                }
+              ></Textarea>
+               <Textarea
+                name="branch4_agree"
+                placeholder="agree"
+                defaultValue={storedBranch1 ? storedBranch1 : ""}
+                borderColor="orange.300"
+                visibility={
+                  topicBranch4 || storedBranch4 ? "visible" : "hidden"
+                }
+              ></Textarea>
+               <Textarea
+                name="branch4_disagree"
+                placeholder="disagree"
+                defaultValue={storedBranch1 ? storedBranch1 : ""}
+                m='2.5rem 0 0 0'
+                borderColor="orange.300"
+                visibility={
+                  topicBranch4 || storedBranch4 ? "visible" : "hidden"
+                }
+              ></Textarea>
+            </Flex>
+
           </Flex>
+
+           {/*last item in flex box*/ }
+          <Flex direction="row" alignContent="center" justifyContent="center" mr='2.5rem'>
+            <Textarea
+                name="branch3_key"
+                placeholder="key"
+                defaultValue={storedBranch1 ? storedBranch1 : ""}
+                m='0 2.5rem 0 0'
+                borderColor="orange.300"
+                visibility={
+                  topicBranch3 || storedBranch3 ? "visible" : "hidden"
+                }
+              ></Textarea>
+               <Textarea
+                name="branch3_agree"
+                placeholder="agree"
+                defaultValue={storedBranch1 ? storedBranch1 : ""}
+                borderColor="orange.300"
+                visibility={
+                  topicBranch3 || storedBranch3 ? "visible" : "hidden"
+                }
+              ></Textarea>
+               <Textarea
+                name="branch3_disagree"
+                placeholder="disagree"
+                defaultValue={storedBranch1 ? storedBranch1 : ""}
+                m='0 0 0 2.5rem'
+                borderColor="orange.300"
+                visibility={
+                  topicBranch3 || storedBranch3 ? "visible" : "hidden"
+                }
+              ></Textarea>
+            </Flex>
           <Flex alignItems="center" justifyContent="flex-end">
             <Button type="submit">Save and continue</Button>
           </Flex>
+
+          </Flex>
         </form>
+
       </Flex>
+      
     </Layout>
   );
 }
