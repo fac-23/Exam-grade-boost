26f31a98cb7ba58df7f55f9a80a7e78260bd9918
Paolo <15671939+paologhidoni@users.noreply.github.com>
2022-03-29 17:54:02 +0100

    Update overviewEssay - relates #91
    Update overviewEssay page to change styling of sections when the user
    has started filling them
    
    Co-authored-by: <Oli 78092825+duckRabbitPy@users.noreply.github.com>
    Co-authored-by: <Adam 31065348+adam8-9@users.noreply.github.com>

--- pages/essayOverview.jsx
+++ pages/essayOverview.jsx
@@ -17,16 +17,36 @@ export async function getServerSideProps({ req }) {
   const essayId = req.cookies.currEssay;
   const essayInfo = await getEssayInfo(essayId);
   const question = essayInfo.question;
+  const spider = essayInfo.spider_1;
+  const introduction = essayInfo.introduction;
+  const body_1 = essayInfo.body_1;
+  const body_2 = essayInfo.body_2;
+  const body_3 = essayInfo.body_3;
+  const conclusion = essayInfo.conclusion;
 
   return {
     props: {
       question,
+      spider,
+      introduction,
+      body_1,
+      body_2,
+      body_3,
+      conclusion,
     },
   };
 }
 
-export default function EssayOverview({ question }) {
-  const { colorMode, toggleColorMode } = useColorMode();
+export default function EssayOverview({
+  question,
+  spider,
+  introduction,
+  body_1,
+  body_2,
+  body_3,
+  conclusion,
+}) {
+  const { colorMode } = useColorMode();
 
   return (
     <Layout>
@@ -37,7 +57,12 @@ export default function EssayOverview({ question }) {
         <form>
           <Flex mt={4} direction="column" gap="1.5rem">
             <Link href="/spiderDiagram" passHref>
-              <Button width="100%" p="2rem 0">
+              <Button
+                width="100%"
+                p="2rem 0"
+                borderColor={spider ? "green" : null}
+                borderWidth="4px"
+              >
                 <Image
                   src={
                     colorMode === "light"
@@ -53,7 +78,12 @@ export default function EssayOverview({ question }) {
               </Button>
             </Link>
             <Link href="/introduction" passHref>
-              <Button width="100%" p="2rem 0">
+              <Button
+                width="100%"
+                p="2rem 0"
+                borderWidth="4px"
+                borderColor="red"
+              >
                 <Image
                   src={colorMode === "light" ? "/intro.svg" : "/intro-dark.svg"}
                   alt="intro icon"
@@ -64,7 +94,7 @@ export default function EssayOverview({ question }) {
               </Button>
             </Link>
             <Link href="/body1" passHref>
-              <Button width="100%" p="2rem 0">
+              <Button width="100%" p="2rem 0" bg={body_1 ? "green" : null}>
                 <Image
                   src={colorMode === "light" ? "/body.svg" : "/body-dark.svg"}
                   alt="body section icon"
@@ -75,7 +105,7 @@ export default function EssayOverview({ question }) {
               </Button>
             </Link>
             <Link href="/body2" passHref>
-              <Button width="100%" p="2rem 0">
+              <Button width="100%" p="2rem 0" bg={body_2 ? "green" : null}>
                 <Image
                   src={colorMode === "light" ? "/body.svg" : "/body-dark.svg"}
                   alt="body section icon"
@@ -87,7 +117,7 @@ export default function EssayOverview({ question }) {
             </Link>
 
             <Link href="/body3" passHref>
-              <Button width="100%" p="2rem 0">
+              <Button width="100%" p="2rem 0" bg={body_3 ? "green" : null}>
                 <Image
                   src={colorMode === "light" ? "/body.svg" : "/body-dark.svg"}
                   alt="body section icon"
@@ -98,7 +128,7 @@ export default function EssayOverview({ question }) {
               </Button>
             </Link>
             <Link href="/conclusion" passHref>
-              <Button width="100%" p="2rem 0">
+              <Button width="100%" p="2rem 0" bg={conclusion ? "green" : null}>
                 <Image
                   src={
                     colorMode === "light"
