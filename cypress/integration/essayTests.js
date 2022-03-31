9f611fb6e8b7f95d592130dc62caf3157010168a
Paolo <15671939+paologhidoni@users.noreply.github.com>
2022-03-31 15:28:01 +0100

    Tests for introduction and body paragrap 1 pages
    Co-authored-by: <Oli 78092825+duckRabbitPy@users.noreply.github.com>
    Co-authored-by: <Adam 31065348+adam8-9@users.noreply.github.com>

--- cypress/integration/essayTests.js
+++ cypress/integration/essayTests.js
@@ -1,11 +1,26 @@
 describe("It should allow the user to write sections of the essay and revisit it", () => {
   let randomString = Math.random().toString(16).slice(8);
 
+  // function to log in
   function logIn() {
     cy.visit("/login");
     cy.get("input[id='email']").type("testing@testing");
     cy.get("input[id='password']").type("testing");
     cy.get("button").contains("Log in").click();
+  }
+
+  function fillAllTxtAreas(section) {
+    let txtAreasArr = cy.get("textarea");
+    txtAreasArr.each((el) => cy.wrap(el).type(randomString));
+
+    cy.get("button").contains("Save and continue").click();
+
+    cy.get("button").contains(section).click();
+
+    let filledTxtAreasArray = cy.get("textarea");
+    filledTxtAreasArray.each((el) =>
+      cy.wrap(el).should("have.text", randomString)
+    );
   }
 
   it("It should allow the enter the essay question and revisit it", () => {
@@ -22,9 +37,22 @@ describe("It should allow the user to write sections of the essay and revisit it
     cy.get("h1").should("contain", `My question ${randomString}`);
   });
 
+  // introduction section
   it("Should allow the user to fill in the introduction fields and revisit them", () => {
     logIn();
     cy.get("button").contains("Edit").first().click();
     cy.get("button").contains("Introduction").click();
+
+    // loop over elements and perform same action on all of them and check the values are there when you come back
+    fillAllTxtAreas("Introduction");
+  });
+
+  // paragraph 1
+  it.only("Should allow the user to fill in the body paragraph 1 fields and revisit them", () => {
+    logIn();
+    cy.get("button").contains("Edit").first().click();
+    cy.get("button").contains("Body paragraph 1").click();
+
+    fillAllTxtAreas("Body paragraph 1");
   });
 });
